6acf1dcb956888da4b72b221198063a71439a254
dilenyprog <20152716@itla.edu.do>
2021-11-13 07:51:02 -0800

    Prueba Commit

--- CitasMedicas/Program.cs
+++ CitasMedicas/Program.cs
@@ -17,6 +17,8 @@ namespace CitasMedicas
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginView());
+
+            //Prueba De cambio
         }
     }
 }
