aba26eb80404a12cbc8cd8f06224e4cf36ef0ec9
dilenyprog <20152716@itla.edu.do>
2021-11-28 19:33:47 -0800

    Actualizacion de metodos en repositorio Generico

--- CitasMedicas/Repositorios/RepositorioGenerico.cs
+++ CitasMedicas/Repositorios/RepositorioGenerico.cs
@@ -5,13 +5,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using CitasMedicas.Datos.Entities;
 using CitasMedicas.Datos.Context;
 
 namespace CitasMedicas.Repositorios
 {
-    class RepositorioGenerico<T>:IGenericoRepositorio<T>
+    public class RepositorioGenerico<T> : IGenericoRepositorio<T> where T : FechaEstatus
     {
-       
+        private AppDbContext _context;
+        private DbSet<T> _set;
 
+
+        public RepositorioGenerico()
+        {
+            _context = new AppDbContext();
+            _set = _context.Set<T>();
+        }
+
+        public T create(T entity)
+        {
+            _set.Add(entity);
+            _context.SaveChanges();
+
+
+            return entity;
+
+        }
+        public OperationResult Delete(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            entity.Borrado = true;
+            entity.FechaModificacion = DateTime.Today;
+
+            _context.SaveChanges();
+            return new OperationResult() { Success = true };
+
+        }
+
+
+
+        public  T  FindById (int Id)
+        {
+            return _set.FirstOrDefault(x => x.Id == Id);
+        }
+        
+
+
+        //public List(T) GetAll()
+        //{
+        //    return _set.Where(x => x.Borrado == false).ToList();
+        //}
+        public OperationResult Update (T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return new OperationResult() { Success = true };
+        }
     }
+
+
 }
